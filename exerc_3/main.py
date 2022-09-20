134b9a37f5102998365379336eda152e9d00000e
Cytorres <cynthiatst85@gmail.com>
2022-09-20 10:58:41 -0300

    oi

---
+++ exerc_3/main.py
@@ -0,0 +1,12 @@
+print('Digite até que número vc quer saber se tem numeros primos:')
+def recebe_valor():
+    valor = int(input('...'))
+    return valor
+
+def calcula_numeros_primos(valor):
+    for numero in range(0,valor+1):
+        return print(numero)
+    
+
+y =recebe_valor()
+x =calcula_numeros_primos(recebe_valor)
